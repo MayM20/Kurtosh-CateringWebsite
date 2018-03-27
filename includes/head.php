542eeca8c7537a0564a1119a8eedd7559b02d958
Gui Rampani <guirampsil@gmail.com>
2018-03-27 05:26:29 +0000

    new files from week 2

---
+++ includes/head.php
@@ -0,0 +1,7 @@
+<head>
+  <title>Page Name</title>
+  <link href="/node_modules/bootstrap/dist/css/bootstrap.css" rel="stylesheet">
+  <script src="/node_modules/jquery/dist/jquery.js"></script>
+  <script src="/node_modules/popper.js/dist/popper.js"></script>
+  <script src="/node_modules/bootstrap/dist/js/bootstrap.js"></script>
+</head>
--- index.php
+++ index.php
@@ -19,17 +19,36 @@ $statement -> execute();
 $result = $statement -> get_result();
 
 //check if there are results
-
-if ($result -> num_rows > 0) {
-    while($row = $result -> fetch_assoc() ){
-        $id = $row["id"];
-        $name = $row["name"];
-        $price = $row["price"];
-        
-        echo "<h1>$name</h1>";
-        echo "<p>$price</p>";
-        echo "<p>Product ID: $id</p>";
-    }
-}
-
 ?>
+<!doctype html>
+<html>
+  <?php include ('includes/head.php'); ?>
+  <body>
+    <?php include ('includes/navbar.php'); ?>
+    <div class="container">
+      <?php 
+      if( $result -> num_rows > 0){
+          $col_counter = 0;
+          while( $row = $result -> fetch_assoc() ){
+              $col_counter++;
+              if ($col_counter == 1){
+                  echo "<div class=\"row\">";
+              }
+              $id = $row["id"];
+              $name = $row["name"];
+              $price = $row["price"];
+              echo "<div class=\"col\">";
+              echo "<h3>$name</h3>";
+              echo "<p>$price</p>";
+              echo "<p>product id: $id</p>";
+              echo "</div>";
+              if ($col_counter == 4){
+                  echo "</div>";
+                  $col_counter = 0;
+              }
+          }
+      }
+      ?>
+    </div>
+  </body>
+</html>
