d050f219ef87bb652596e9192f3c1878df8d856f
Gui Rampani <guirampsil@gmail.com>
2018-05-15 03:28:00 +0000

    changes

--- classes/account.class.php
+++ classes/account.class.php
@@ -9,6 +9,11 @@ class Account extends Database{
     if( strlen( trim($username) ) < 4 ){
       $errors["username"] = "at least 4 characters";
     }
+    
+    if ($this -> checkUserName($username)){
+      $errors["username"] = $errors["username"] . " " . "username already in use";
+    }
+    
     //validate the email
     if( filter_var($email, FILTER_VALIDATE_EMAIL ) == false ){
       $errors["email"] = "invalid email address";
@@ -39,8 +44,29 @@ class Account extends Database{
     }
     else{
       //process errors
+      $this -> errors = $errors;
+      return false;
     }
     
+  }
+  
+  public function checkUserName($username){
+    // check if username is already in the db
+    //true if it does exist, false if it doesn't
+    $query = "SELECT username FROM accounts WHERE username = ?";
+    $statement = $this -> connection -> prepare($query);
+    $statement -> bind_param("s", $username);
+    $statement -> execute();
+    $result = $statement -> get_result();
+    
+    if ($result -> num_rows > 0){
+      //username exists
+      return true;
+    }
+    else {
+      //username does not exist
+      return false;
+    }
   }
 }
 ?>
--- register.php
+++ register.php
@@ -62,7 +62,7 @@ $page_title = "Register for an account";
       <div class="row justify-content-center">
         <div class="col-5">
           <h2>Register for an account</h2>
-          <form method="post" action="register.php">
+          <form id="register-form" method="post" action="register.php">
             <div class="form-group">
               <label for="username">User Name</label>
               <input id="username" name="username" class="form-control" placeholder="username" type="text">
@@ -76,13 +76,73 @@ $page_title = "Register for an account";
               <input id="password" name="password" class="form-control" placeholder="password" type="password">
             </div>
             <div class="text-center">
-              <button class="btn btn-primary">
-                Sign up
+              <button type="submit" class="btn btn-outline-primary" name="register-btn">
+                Register
               </button>
             </div>
           </form>
         </div>
       </div>
     </div>
+    <script>
+      $(document).ready(
+        () => {
+          $('#register-form').on('submit', (event) => {
+            //event.preventDefault();
+            let username = $('input[name="username"]').val();
+            let email = $('input[name="email"]').val();
+            let password = $('input[name="password"]').val();
+            let registerdata = {username: username, email: email, password: password};
+            let spinner = '<img class="spinner" src="/images/graphics/spinner.gif">';
+            $('button[name="register-btn"]').append(spinner);
+            $('button[name="register-btn"]').attr('disabled','');
+            //console.log(username,email,password);
+            
+            $.ajax({
+              url: '/ajax/register.ajax.php',
+              method: 'post',
+              dataType: 'json',
+              data: registerdata
+            })
+            .done((response) => {
+              console.log(response);
+              //remove spinner
+              $('button[name="register-btn"] img').remove();
+              $('.alert').remove();
+              if (response.success == false){
+                //check for errors in different fields
+                if (response.errors.username){
+                  let template = $('#register-error').html().trim();
+                  let clone = $(template);
+                  $(clone).find('.alert-message').text(response.errors.username);
+                  $('input[name="username"]').parents('.form-group').append(clone);
+                }
+                if (response.errors.email){
+                  let template = $('#register-error').html().trim();
+                  let clone = $(template);
+                  $(clone).find('.alert-message').text(response.errors.email);
+                  $('input[name="email"]').parents('.form-group').append(clone);
+                }
+                if (response.errors.password){
+                  let template = $('#register-error').html().trim();
+                  let clone = $(template);
+                  $(clone).find('.alert-message').text(response.errors.password);
+                  $('input[name="password"]').parents('.form-group').append(clone);
+                }
+              }
+              $('button[name="register-btn"]').removeAttr('disabled');
+            });
+          });
+        }
+      );
+    </script>
   </body>
 </html>
+<template id="register-error">
+  <div class="alert alert-warning alert-dismissible fade show" role="alert">
+        <span class="alert-message"></span>
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+</template>
